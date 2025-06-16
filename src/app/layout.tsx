97c67aeb4cd9c31517786612534bc363896c5d2b
Amlan Das <das.amlan1320@gmail.com>
2025-06-16 20:07:22 +0000

    update layout description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: 'Amlan Das - Personal Portfolio',
-  description: 'This is my personal space to showcase my journey, projects, and learnins',
+  description: 'This is my personal space to showcase my journey, projects, and learnings',
 };
 
 export default function RootLayout({
